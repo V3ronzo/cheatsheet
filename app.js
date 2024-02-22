850abc6d03e1644661176047b29e0f4e4e9fb0cc
Veronzo Hurd <vzo@Veronzos-iMac.local>
2024-02-22 15:10:17 -0500

    CheatSheet

---
+++ app.js
@@ -0,0 +1,7 @@
+
+// keyword --- name ---   () => {}
+const changeButton =  () => {
+    // this is where i put my instructions
+    let container = document.querySelector('.lowContainer')
+   container.innerHTML=" HEY! THIS WORKS"
+}   
